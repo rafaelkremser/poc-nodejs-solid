ac2caa05a241c33335aaa691fdfb5f3a022c17d5
Rafael Freitas Kremser <rafaelfreitaskermser@gmail.com>
2024-06-10 23:02:26 -0300

    test: create gyms e2e test

---
+++ src/http/controllers/gyms/create.spec.ts
@@ -0,0 +1,31 @@
+import request from 'supertest';
+import { app } from '@/app';
+import { afterAll, beforeAll, describe, expect, it } from 'vitest';
+import { createAndAuthenticateUser } from '@/utils/test/createAndAuthenticateUser';
+
+describe('Create Gym (e2e)', () => {
+    beforeAll(async () => {
+        await app.ready();
+    });
+
+    afterAll(async () => {
+        await app.close();
+    });
+
+    it('should be able to create a gym', async () => {
+        const { token } = await createAndAuthenticateUser(app);
+
+        const createGymResponse = await request(app.server)
+            .post('/gyms')
+            .set('Authorization', `Bearer ${token}`)
+            .send({
+                title: 'JavaScript Gym',
+                description: null,
+                phone: null,
+                latitude: 16.1471697,
+                longitude: -40.2968506,
+            });
+
+        expect(createGymResponse.statusCode).toEqual(201);
+    });
+});
