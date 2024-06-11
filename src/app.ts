348f6adeed05d89b93bbbdd185752d992c6d8e8b
Rafael Freitas Kremser <rafaelfreitaskermser@gmail.com>
2024-06-11 16:27:04 -0300

    feat: implements refresh token

--- src/app.ts
+++ src/app.ts
@@ -4,12 +4,22 @@ import { gymsRoutes } from './http/controllers/gyms/routes';
 import { ZodError } from 'zod';
 import { env } from './env';
 import fastifyJwt from '@fastify/jwt';
+import fastifyCookie from '@fastify/cookie';
 import { checkInsRoutes } from './http/controllers/checkIns/routes';
 export const app = fastify();
 
 app.register(fastifyJwt, {
     secret: env.JWT_SECRET,
+    cookie: {
+        cookieName: 'refreshToken',
+        signed: false,
+    },
+    sign: {
+        expiresIn: '10m',
+    },
 });
+
+app.register(fastifyCookie);
 
 app.register(usersRoutes);
 app.register(gymsRoutes);
